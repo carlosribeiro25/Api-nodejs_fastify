e91f5cc3bfc4b34ac65fec300c0cde926a61a3a8
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2025-12-08 04:15:51 -0300

    add variable in file server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import {server} from './app'
 
+const port = process.env.PORT || 3333
+
 server.listen({ port: 3333, host : "0.0.0.0" }).then(() => {
-    console.log("HTTP server runing!")
+    console.log(`HTTP server runing in port ${port}`)
 })
